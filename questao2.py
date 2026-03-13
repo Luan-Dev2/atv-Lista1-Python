577b03f42b770245c5f168ed6140158aa2c259a6
Luan Santos <luanssantos1228@gmail.com>
2026-03-12 21:26:13 -0300

    Questão 2 criada!

---
+++ questao2.py
@@ -0,0 +1,3 @@
+salario_funcionario = float(input('Informe o salário atual do funcionário: R$'))
+novo_salario = salario_funcionario + (salario_funcionario * 35/100)
+print(f'O salário do funcionário passará a ser R${novo_salario:.2f} com 35% de aumento.')
